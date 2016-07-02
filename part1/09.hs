246aea964feec00893fc3238aee6c7c8f736b208
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-07-02 14:02:23 +0900

    update

--- part1/09.hs
+++ part1/09.hs
@@ -1,44 +1,36 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-import qualified Data.HashTable.IO as Hash
 import qualified System.Random as R
+import Data.List (partition)
+
+main :: IO ()
+main =  ( f $ sep dist) >>= putStrLn
+
+dist :: String
 dist = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
-type Tbl k v = Hash.BasicHashTable k v
 
-type Map = (Int, String)
+type D = (String, Int)
 
-main = (f dist) >>= putStrLn
+type Data = ([D], [D])
 
-f :: String -> IO String
-f s = do
-  h1 <- Hash.new
-  h2 <- Hash.new
-  h1' <- h h1 r
-  h2' <- h h2 l
-  construct h1' h2'
+sep :: String -> Data
+sep s = partition (\ (x, _) -> (length x) <= 4) $ zip ( words s ) [1..]
+
+f :: Data -> IO String
+f d = do
+  gen <- R.newStdGen
+  g gen 0  d
   where
-    (r, l) = g ([],[]) (words s) 1
-    h :: (Tbl Int String) -> [Map] -> IO (Tbl Int String)
-    h t [] = return t
-    h t ((i,s):ms) = do
-      Hash.insert t i s
-      h t ms
-
-
-
-g :: Int -> ([Map],[Map]) -> [String] -> Int -> ([Map], [Map])
-g m [] _ =  m
-g (m,n) (x:xs) i 
-      | (check x) = g (((i,x):m , n)) xs (i+1)
-      | otherwise = g ((m , (i,x):n)) xs (i+1)
+    g :: R.StdGen -> Int -> Data -> IO [Strinb]
+    g _ _ [] [] = return []
+    g _ _ xs [] = xs
+    g gen i [] ys = (get gen ys) : (g gen' (i+1) ys')
       where
-        check :: String -> Bool
-        check = (<= 4) . length
+        (y, ys', gen') = get gen ys
+    g gen i (x:xs) y = undefined
 
-
-construct :: (Tbl Int String) -> (Tbl Int String) -> IO [String]
-construct h j = do
-  g <- R.newStdGen
-  f 0 h j 
-  where f :: R.StdGen -> Int -> (Tbl Int String) -> (Tbl Int String) -> IO [String]
-
+get :: R.StdGen -> [a] -> (a, [a], R.StdGen)
+get f xs = undefined
+  where
+    (i,gen) = R.randomR (0,length xs -1)
+    take :: [a] -> Int -> (a,[a])
+    take x i = undefined
